60f442b4160d445b5c5d282fbabc4b6e518d7935
Gal009 <92556891+Gal009@users.noreply.github.com>
2021-12-04 20:53:23 -0300

    Yeah, boring code stuff.
    I was tired and couldn't resolve a simple error on the code, but i think it's working now.

---
+++ Handlers/Player2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.EventArgs;
+using Exiled.API.Features;
+using Exiled.API.Extensions;
+using Exiled.API.Features.Components;
+using CustomPlayerEffects;
+using ExEvents = Exiled.Events.Handlers;
+using Exiled.API.Enums;
+using MEC;
+using Exiled.API;
+using Exiled.Events;
+
+
+namespace ChorãoUtilities.Handlers
+{
+    public class Player2
+    {
+
+        public void OnEnraging(EnragingEventArgs ev)
+        {
+            ev.Player.Broadcast(6, Plugin.Singleton.Config.AngryMessage);
+            ev.Player.MaxArtificialHealth = Plugin.Singleton.Config.Max096AHPInRage;
+            ev.Player.MaxHealth = Plugin.Singleton.Config.MaxHealth096InRage;
+        }
+
+        public void OnCalmingDown(CalmingDownEventArgs ev)
+        {
+            ev.Player.ShowHint(Plugin.Singleton.Config.CalmingDownMessage, 5);
+            ev.Player.MaxArtificialHealth = Plugin.Singleton.Config.Max096AHPAfterCalmingDown;
+            ev.Player.MaxHealth = Plugin.Singleton.Config.MaxHealth096CalmingDown;
+        }
+
+        public void OnAddingTarget(AddingTargetEventArgs ev)
+        {
+            ev.EnrageTimeToAdd = Plugin.Singleton.Config.TimeThat096GainsOfRageWhenSomeoneLooks;
+        }
+
+        public void OnEnraging096(AddingTargetEventArgs ev)
+        {
+            ev.Target.ShowHint(Plugin.Singleton.Config.LookedTo096);
+            ev.Target.EnableEffect(EffectType.BodyshotReduction, Plugin.Singleton.Config.PanicTime);
+        }
+
+        public void OnWalkingOnTantrum(WalkingOnTantrumEventArgs ev)
+        {
+            if (ev.Player.IsHuman)
+            {
+                ev.Player.ShowHint(Plugin.Singleton.Config.WalkingOnTantrumMessage, 5);
+                ev.Player.EnableEffect(EffectType.Poisoned, Plugin.Singleton.Config.PeanutPoisoningWalkingOnTantrumTime);
+            }
+        }
+
+        public void OnPlacingTantrum(PlacingTantrumEventArgs ev)
+        {
+            ev.Player.EnableEffect(EffectType.Scp207, Plugin.Singleton.Config.PeanutPlacingTantrumCokeTime);
+            ev.Player.ChangeEffectIntensity<Scp207>(Plugin.Singleton.Config.CokeIntensityWhenPeanutPlacesTantrum);
+        }
+
+        public void OnEnteringFemurBreaker(EnteringFemurBreakerEventArgs ev)
+        {
+            if (ev.Player.IsHuman)
+            {
+                ev.Player.ShowHint(Plugin.Singleton.Config.EnteringOnFemurBreaker, 5);
+            }
+        }
+
+        public void OnTogglingNoClip(TogglingNoClipEventArgs ev)
+        {
+            ev.Player.ShowHint("Adm Abusus", 5);
+        }
+
+        public void OnGettingHurt(HurtingEventArgs ev)
+        {
+            if (ev.Handler.Type == DamageType.Decontamination)
+            {
+                ev.Target.ShowHint(Plugin.Singleton.Config.NoobDiedtoDecontamination, 5);
+            }
+        }
+
+        public void OnHurtingPLayer(HurtingEventArgs ev)
+        {
+            if (ev.Handler.Type == DamageType.Scp) 
+            if (ev.Attacker.Role == RoleType.Scp93989)
+            {
+                    ev.Target.ShowHint(Plugin.Singleton.Config.BleedingMessage);
+                    ev.Target.EnableEffect(EffectType.Bleeding, Plugin.Singleton.Config.BleedingTime);
+            }
+        }
+    }
+}
+    
+
+
---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Enums;
+using PlayableScps;
+using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using MEC;
+using Server = Exiled.Events.Handlers.Server;
+using Player = Exiled.Events.Handlers.Player;
+using Exiled.API.Extensions;
+using Exiled.API.Features.Components;
+using CustomPlayerEffects;
+using ExEvents = Exiled.Events.Handlers;
+
+namespace ChorãoUtilities
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance => Singleton;
+
+        public override PluginPriority Priority { get; } = PluginPriority.Lowest;
+        public Handlers.Server2 server;
+        public Handlers.Player2 player;
+        public static Plugin Singleton;
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            RegisterEvents();
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Singleton = null;
+            UnRegisterEvents();
+
+            base.OnDisabled();
+        }
+
+        public void RegisterEvents()
+        {
+            server = new Handlers.Server2();
+            player = new Handlers.Player2();
+
+
+            Exiled.Events.Handlers.Scp096.AddingTarget += player.OnEnraging096;
+            Player.WalkingOnTantrum += player.OnWalkingOnTantrum;
+            Player.EnteringFemurBreaker += player.OnEnteringFemurBreaker;
+            ExEvents.Scp096.Enraging += player.OnEnraging;
+            ExEvents.Scp096.AddingTarget += player.OnAddingTarget;
+            ExEvents.Scp096.CalmingDown += player.OnCalmingDown;
+            ExEvents.Scp173.PlacingTantrum += player.OnPlacingTantrum;
+            Player.Hurting += player.OnGettingHurt;
+            Player.Hurting += player.OnHurtingPLayer;
+        }
+
+        public void UnRegisterEvents()
+        {
+
+            Exiled.Events.Handlers.Scp096.AddingTarget -= player.OnEnraging096;
+            Player.WalkingOnTantrum -= player.OnWalkingOnTantrum;
+            ExEvents.Scp096.Enraging -= player.OnEnraging;
+            ExEvents.Scp096.AddingTarget -= player.OnAddingTarget;
+            ExEvents.Scp096.CalmingDown -= player.OnCalmingDown;
+            ExEvents.Scp173.PlacingTantrum -= player.OnPlacingTantrum;
+            Player.Hurting -= player.OnGettingHurt;
+            Player.Hurting -= player.OnHurtingPLayer;
+
+            player = null;
+            server = null;
+        }
+    }
+}
